9462422925c89cd4cb30798d3ecfafc210ea7805
Adrian Covarrubias <adriancova.95@gmail.com>
2024-01-15 21:27:57 -0800

    NavBar with links

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -7,14 +7,32 @@
 	import Logo from './Logo.svelte';
 
 	let { globalClicks } = $props<{ globalClicks: number }>();
+
+	const navItems = [
+		{ label: 'Inicio', href: '/' },
+		{ label: 'Tus Tallies & Grupos', href: '/tallies' },
+		{ label: 'Global', href: '/global' }
+	];
 </script>
 
 <header
 	class="sticky top-0 z-50 w-full bg-border shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
-	<div class={`flex h-14 items-center sm:container max-sm:px-4 md:h-32 `}>
+	<div class={`flex h-14 items-center justify-between sm:container max-sm:px-4 sm:h-32 `}>
 		<Logo {globalClicks} />
-		<div class="ml-auto flex items-center justify-end gap-2">
+
+		<div class="mx-auto max-sm:hidden">
+			<nav
+				class="text-md mx-auto flex items-center space-x-6 font-medium max-md:flex-wrap lg:space-x-16 lg:text-lg"
+			>
+				{#each navItems as item}
+					<a href={item.href} class="transition-colors">
+						{item.label}
+					</a>
+				{/each}
+			</nav>
+		</div>
+		<div class="flex items-center justify-end gap-2 max-sm:ml-auto">
 			{#if $page.session}
 				<form method="post" action="/auth/sign-out" use:enhance>
 					<Button type="submit" variant="destructive" size="sm">Cerrar sesión</Button>
