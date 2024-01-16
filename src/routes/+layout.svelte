f0111fdfa8236165e28f87c722eaec13e7a1615f
Adrian Covarrubias <adriancova.95@gmail.com>
2024-01-15 16:36:28 -0800

    replaced sonner with svelte-toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { ModeWatcher } from 'mode-watcher';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import NavBar from './../lib/components/NavBar.svelte';
 	import Footer from './../lib/components/Footer.svelte';
 	import '../app.pcss';
 </script>
 
 <ModeWatcher />
+<SvelteToast />
 <div class="relative flex min-h-screen flex-col" id="page">
 	<NavBar />
 	<div class="flex-1">
@@ -13,3 +15,12 @@
 	</div>
 	<Footer />
 </div>
+
+<style>
+	:root {
+		--toastContainerTop: 2rem;
+		--toastContainerRight: auto;
+		--toastContainerBottom: auto;
+		--toastContainerLeft: calc(50vw - 8rem);
+	}
+</style>
