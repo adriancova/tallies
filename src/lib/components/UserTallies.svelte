bb9d166916bbe35ef65f40b6a37f4c1e2ef2d979
Adrian Covarrubias <adriancova.95@gmail.com>
2024-01-15 20:38:00 -0800

    userTallies with cols-3

--- src/lib/components/UserTallies.svelte
+++ src/lib/components/UserTallies.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import Autoplay from 'embla-carousel-autoplay';
-	import * as Carousel from '$lib/components/ui/carousel/index.js';
 	import TallyCard from '$lib/components/TallyCard.svelte';
 
 	let { tallies } = $props<{ tallies: any[] }>();
@@ -8,6 +6,8 @@
 
 <h3 class="my-8 text-center text-2xl font-bold text-primary">Tus Tallies</h3>
 
-{#each tallies as { tallyTitle, tallyDescription }, i (i)}
-	<TallyCard {tallyTitle} {tallyDescription} />
-{/each}
+<div class="w-full grid-cols-3 gap-4">
+	{#each tallies as { tallyTitle, tallyDescription }, i (i)}
+		<TallyCard {tallyTitle} {tallyDescription} />
+	{/each}
+</div>
