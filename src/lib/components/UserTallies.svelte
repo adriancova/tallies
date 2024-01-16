46c6c808c4caac77040ac277cf1683ea302dc946
Adrian Covarrubias <adriancova.95@gmail.com>
2024-01-15 18:21:46 -0800

    cleaned up base page with multiple components

---
+++ src/lib/components/UserTallies.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Autoplay from 'embla-carousel-autoplay';
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import TallyCard from '$lib/components/TallyCard.svelte';
+
+	let { tallies } = $props<{ tallies: any[] }>();
+</script>
+
+<h3 class="my-8 text-center text-2xl font-bold text-primary">Tus Tallies</h3>
+
+{#each tallies as { tallyTitle, tallyDescription }, i (i)}
+	<TallyCard {tallyTitle} {tallyDescription} />
+{/each}
