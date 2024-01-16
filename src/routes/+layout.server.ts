24bd838bef3ac97ba39a8f263e7d92567fef47ba
Adrian Covarrubias <adriancova.95@gmail.com>
2024-01-15 22:23:31 -0800

    simulating load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,8 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async (event) => {
 	const serverTallies = [];
 	if (event.locals?.session) {
-		serverTallies.push('server tallies');
+		const sT = [] as string[];
+		serverTallies.push(...sT);
 	}
 	return { serverTallies };
 };
